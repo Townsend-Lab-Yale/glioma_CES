efb65d69486b24ae2eb9a69e9cb5974eeba0dfbe
vcannataro <cannatarov@emmanuel.edu>
2020-08-14 12:15:25 -0400

    updated annotation filtering

--- analysis_scripts/effect_size_results_combiner.R
+++ analysis_scripts/effect_size_results_combiner.R
@@ -20,7 +20,7 @@ library(tidyverse)
 
 for(j in 1:length(cores_files)){
   
-  # load("../cluster_work/results/UCEC_selection_output_ML_cores.RData")
+  
   load(cores_files[j])
   
   # selection_data <- cancereffectsizeR::selection_results_converter(analysis)
@@ -30,34 +30,47 @@ for(j in 1:length(cores_files)){
   selection_data <- as.data.frame(analysis@selection_results[tumors_with_variant > 1])
   # getting rid of double annotations
   
-  annot_number <- sapply(analysis@maf$assoc_aa_mut,length)
+  annot_number <- sapply(analysis@maf$genes,length)
   annot_index <- which(annot_number > 1)
   if(length(annot_index)>0){
     for(annot in 1:length(annot_index)){
-      # if(annot == 88){break}
       
-      # if(!"ABHD14A-ACY1_L354P_ENSP00000420487" %in% selection_data$variant){break}
       
-      these_variants <- unlist(analysis@maf$assoc_aa_mut[annot_index[annot]])
-      
-      # if("ABHD14A-ACY1_L354P_ENSP00000420487" %in% these_variants){break}
-      
-      selection_ind <- which(selection_data$variant %in% these_variants)
-      selection_pick <- which.max(selection_data$selection_intensity[selection_ind])
-      winner <- selection_ind[selection_pick]
-      
-      winner_variant <- selection_data$variant[winner]
-      
-      winner_maf_ind <- which(analysis@maf$assoc_aa_mut[annot_index[annot]][[1]] == winner_variant)
-      
-      analysis@maf$assoc_aa_mut[annot_index[annot]][[1]] <- analysis@maf$assoc_aa_mut[annot_index[annot]][[1]][winner_maf_ind]
-      analysis@maf$genes[annot_index[annot]][[1]] <- analysis@maf$genes[annot_index[annot]][[1]][winner_maf_ind]
-      
-      if(length(selection_ind[-selection_pick])>0){
-        selection_data <- selection_data[-selection_ind[-selection_pick],]
+      # just deduplicate recurrent variants (that is all we have effect size information from) 
+      if(length(which(analysis@maf$snv_id==analysis@maf$snv_id[annot_index[annot]]))>1){
+        # ZNF321P_N42H_ENSP00000375656
+        
+        # if the variant is non-coding... 
+        if(all(is.na(unlist(analysis@maf$assoc_aa_mut[annot_index[annot]])))){
+          
+          # pick first gene for the annotation
+          
+          analysis@maf$genes[annot_index[annot]] <- unlist(analysis@maf$genes[annot_index[annot]])[1]
+          
+          
+        }else{ # if the variant has an associated amino acid change 
+          these_variants <- unlist(analysis@maf$assoc_aa_mut[annot_index[annot]])
+          # if("19:53472796_T>A" %in% analysis@maf$snv_id[annot_index[annot]]){break}
+          # if(unlist(analysis@maf$genes[annot_index[annot]])  == c("ZNF321P", "ZNF816")){break}
+          
+          selection_ind <- which(selection_data$variant %in% these_variants)
+          selection_pick <- which.max(selection_data$selection_intensity[selection_ind])
+          winner <- selection_ind[selection_pick]
+          
+          winner_variant <- selection_data$variant[winner]
+          
+          winner_maf_ind <- which(analysis@maf$assoc_aa_mut[annot_index[annot]][[1]] == winner_variant)
+          
+          analysis@maf$assoc_aa_mut[annot_index[annot]][[1]] <- analysis@maf$assoc_aa_mut[annot_index[annot]][[1]][winner_maf_ind]
+          analysis@maf$genes[annot_index[annot]][[1]] <- analysis@maf$genes[annot_index[annot]][[1]][winner_maf_ind]
+          
+          if(length(selection_ind[-selection_pick])>0){
+            selection_data <- selection_data[-selection_ind[-selection_pick],]
+          }
+          
+        }
+        # if(nrow(selection_data) == 0){break}
       }
-      # if(nrow(selection_data) == 0){break}
-      
       
     }
   }
@@ -88,17 +101,23 @@ for(j in 1:length(cores_files)){
   selection_data$Gene[coding_ind] <- sapply(selection_data_var_split[coding_ind], function(x) paste(x[1]))
   # assign gene names to noncoding 
   
+  
+  # [1] "ZNF321P" "ZNF816"  problem
+  
   for(this_row in 1:nrow(selection_data)){
     if(is.na(selection_data$Gene[this_row])){
       # selection_data$Gene <- analysis$mutations$snv[selection_data$variant, genes]
       
+      # if(length(unlist(analysis@maf[which(analysis@maf$snv_id == selection_data$variant[this_row])[1],genes]))>1){
+      # break
+      # }
       # should always be one because we got rid of duplicate annotations above
       selection_data$Gene[this_row] <- unlist(analysis@maf[which(analysis@maf$snv_id == selection_data$variant[this_row])[1],genes])
       
     }
   }
   
-
+  
   
   selection_data$dndscv_q <- NA
   
@@ -108,7 +127,7 @@ for(j in 1:length(cores_files)){
   }
   
   
-
+  
   selection_data$tumor_type <- tumor_names[j]
   # dndscv_results <- selection_output$dndscvout$sel_cv
   # rownames(dndscv_results) <- dndscv_results$gene_name
